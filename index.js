e998392fc134435f93657c29fca0547c8a167017
root <root@vpn300.vpnservice.company>
2019-02-20 18:24:58 -0500

    working module

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 var l = console.log,
     _ = require('underscore'),
-    child = require('child_process');
+    child = require('child_process'),
+    c = require('chalk');
+
+var Interface = {
+    socketPath: '/var/run/occtl.socket',
+    occtlPath: '/usr/bin/occtl',
+    connect: function(socketPath) {
+
+        var ocCmd = this.occtlPath + ' -js' + this.socketPath;
+        l(c.green('Running Command ') + c.white.bgBlack(ocCmd));
+
+
+
+    },
+
+};
+
+
+module.exports = Interface;
