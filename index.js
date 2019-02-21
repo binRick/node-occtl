95018d43cff36c3258029987665305f5fcf7e6bf
binRick <root@vpn300.vpnservice.company>
2019-02-20 21:05:22 -0500

    working openconnect status parser, test cli script

--- index.js
+++ index.js
@@ -1,21 +1,91 @@
 var l = console.log,
     _ = require('underscore'),
     child = require('child_process'),
-    c = require('chalk');
+    c = require('chalk'),
+    pj = require('prettyjson'),
+    parse = require('parse-spawn-args').parse;
+
 
 var Interface = {
     socketPath: '/var/run/occtl.socket',
     occtlPath: '/usr/bin/occtl',
-    connect: function(socketPath) {
-
-        var ocCmd = this.occtlPath + ' -js' + this.socketPath;
-        l(c.green('Running Command ') + c.white.bgBlack(ocCmd));
-
-
-
+    debug: false,
+    genCmd: function(args) {
+        return parse('-js' + this.socketPath + ' ' + args);
     },
+    showValidSessions: function(_cb) {
+        this.runCommand('show sessions valid', _cb);
+    },
+    showAllSessions: function(_cb) {
+        this.runCommand('show sessions all', _cb);
+    },
+    showUsers: function(_cb) {
+        this.runCommand('show users', _cb);
+    },
+    showStatus: function(_cb) {
+        this.runCommand('show status', _cb);
+    },
+    runCommand: function(command, _cb) {
+        var ocCmd = this.genCmd(command);
+        childProc = child.spawn(this.occtlPath, ocCmd),
+            outData = '';
+        if (this.debug)
+            l(c.green('Running Command ') + c.white.bgBlack(ocCmd));
+        childProc.stderr.on('data', function(out) {
+            l('stderr>', out.toString());
+        });
+        childProc.stdout.on('data', function(out) {
+            outData += out.toString();
+            if (this.debug)
+                l('stdout>', out.toString());
+        });
+        childProc.on('exit', function(code) {
+            if (this.debug)
+                l('childProc exited w code', code);
+            if (code == 0) {
+                try {
+                    outData = JSON.parse(fixOpenConnectInvalidJson(outData));
+                    if (this.debug)
+                        l(pj.render(outData));
+                    _cb(null, outData);
+                } catch (e) {
+                    if (this.debug) {
+                        l(c.red('Unable to parse JSON from string:'));
+                        l(outData);
+                    }
+                    _cb(e);
+                }
+
+            }
+        });
+    },
+
 
 };
 
 
+
+
+var fixOpenConnectInvalidJson = function(outData) {
+    outData = outData.trim();
+    outDataNew = '';
+    _.each(outData.split("\n"), function(line) {
+        line = line.trim();
+        if (line[line.length - 1] != ',' && line.split('"').length > 2)
+            line += ',';
+        outDataNew += line + "\n";
+    });
+    outData = outDataNew;
+    var outDataLines = outData.split("\n").filter(function(line) {
+        return line.length;
+    });
+    var lastJsonLine = outDataLines[outDataLines.length - 2];
+    if (lastJsonLine[lastJsonLine.length - 1] == ',')
+        lastJsonLine = lastJsonLine.slice(0, lastJsonLine.length - 1);
+
+    outDataLines[outDataLines.length - 2] = lastJsonLine;
+
+    outData = outDataLines.join('\n');
+    return outData;
+};
 module.exports = Interface;
--- test.js
+++ test.js
@@ -1,9 +1,31 @@
 #!/usr/bin/env node
-var occtl = require('./');
+
+var occtl = require('./'),
+    l = console.log,
+    pj = require('prettyjson');
+c = require('chalk');
 
 occtl.socketPath = '/var/run/occtl.socket';
 occtl.occtlPath = '/usr/bin/occtl';
+occtl.debug = false;
 
-
-occtl.connect();
-
+occtl.showStatus(function(e, ocStatus) {
+    if (e) throw e;
+    l('ocStatus=', ocStatus.Status);
+    l(pj.render(ocStatus));
+    occtl.showUsers(function(e, ocUsers) {
+        if (e) throw e;
+        l(c.red('\n\nUsers Connected=') + ' ' + c.white(ocUsers.length));
+        l(pj.render(ocUsers));
+        occtl.showAllSessions(function(e, ocAllSessions) {
+            if (e) throw e;
+            l(c.red('\n\nTotal Sessions=') + ' ' + c.white(ocAllSessions.length));
+            l(pj.render(ocAllSessions));
+            occtl.showValidSessions(function(e, ocValidSessions) {
+                if (e) throw e;
+                l(c.red('\n\nValid Sessions=') + ' ' + c.white(ocValidSessions.length));
+                l(pj.render(ocValidSessions));
+            });
+        });
+    });
+});
